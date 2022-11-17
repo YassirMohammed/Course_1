1d6c3b947525be5e0243be7d138535efd517dfa5
YassirMohammed <yassirm0033@gmail.com>
2022-11-17 19:56:30 +0200

    Name, age and address (4th ass.)

---
+++ 4th assignment - 1st part.py
@@ -0,0 +1,17 @@
+# Welcome
+print('Hi there!, please add the info requested below...')
+
+# inputs
+name = input("Enter your name: ")
+if name.isspace() or name.isdigit():
+    input("Input wrong, click Enter to exit...")
+    exit()
+age = input("Enter your age: ")
+if age.isspace():
+    input("Input wrong, click Enter to exit....")
+    exit()
+age_number = int(age)
+
+address = input("Enter your address: ")
+
+print("Hello Mr./Ms.", name, ".", "Your age is", age_number, "and your address is: ", address)
