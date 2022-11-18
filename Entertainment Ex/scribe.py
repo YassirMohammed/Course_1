d17bfb7d4edcacd1ff520896694a907d9efa3f3f
YassirMohammed <yassirm0033@gmail.com>
2022-11-18 23:20:31 +0200

    Updating document place

---
+++ Entertainment Ex/scribe.py
@@ -0,0 +1,9 @@
+
+def drawSquare(size):
+    horiz1 = print(".  .  .  .  .")
+    vert_points = [".", ".", ".", ".", "."]
+    for point in vert_points:
+        print(point)
+    horiz2 = print(".  .  .  .  .")
+
+drawSquare(5)
