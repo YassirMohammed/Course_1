68984372ff3700b925878b0732f0f3df60157c8f
YassirMohammed <yassirm0033@gmail.com>
2022-11-20 12:53:10 +0200

    adding float issue

--- GSG/4th assignment - 1st part.py
+++ GSG/4th assignment - 1st part.py
@@ -4,9 +4,17 @@ print('Hi there!, please add the info requested below...')
 # inputs
 name = input("Enter your name: ")
 if name.isspace() or name.isdigit():
-    input("Input wrong, click Enter to exit...")
+    input("Input is space or int, click Enter to exit...")
     exit()
+
+if not name.isspace():
+    name_to_float = float(name)
+    if type(name_to_float) == float:
+        input("Input float, click Enter to exit...")
+        exit()
+    
 age = input("Enter your age: ")
+
 if age.isspace():
     input("Input wrong, click Enter to exit....")
     exit()
